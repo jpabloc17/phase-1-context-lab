69c63506074591a1b60657e2e4543db3f22889f3
Pablo Castro <pablocas-tro1@outlook.com>
2023-04-28 13:02:29 -0500

    completed Lab

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array){
+    return array.map((element) => {
+        return createEmployeeRecord(element);
+    });
+};
+
+function createTimeInEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    });
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date,
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    const timeIn = this.timeInEvents.find(function(element){
+        return element.date === dateStamp
+    });    
+    const timeOut = this.timeOutEvents.find(function(element){
+        return element.date === dateStamp
+    });
+
+    return (timeOut.hour - timeIn.hour) / 100;
+};
+
+function wagesEarnedOnDate(dateStamp){
+    const hoursWorked = hoursWorkedOnDate.call(this, dateStamp) 
+    return hoursWorked * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(function(element){
+        return element.firstName === firstName;
+    });
+};
+
+function calculatePayroll(arrayOfEmployee){
+    const totalPayroll = arrayOfEmployee.reduce(function(total, employeeRecord){
+        return total + allWagesFor.call(employeeRecord);
+    }, 0);
+        return totalPayroll;
+}
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
